ca14a9a5deae0a00c44ca60836c8bfba7ba60831
Tania Lapa <taniamiroshnuchenko@gmail.com>
2026-06-30 12:41:54 +0200

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,9 +31,11 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+         <Providers>
         <Header />
-        <Providers>{children}</Providers>
-        <Footer />
+        {children}
+            <Footer />
+            </Providers>
       </body>
     </html>
   );
